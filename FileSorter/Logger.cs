7e41456a8db4b0e3f2236f86e041db7394473777
Artur Czołgosz (Czolgmen) <arturczolg@gmail.com>
2025-11-14 12:27:24 +0100

    Initial logger work done

--- FileSorter/Logger.cs
+++ FileSorter/Logger.cs
@@ -1,6 +1,50 @@
 namespace FileSorter;
 
-public class Logger
+
+public interface ILogger
 {
-    
+    void Info(string message);
+    void Warn(string message);
+    void Error(string message, Exception ex);
+    void Debug(string message);
+}
+
+
+public class Logger(Stream consoleStream, bool writeDebug = false) : ILogger
+{
+    private readonly StreamWriter _writer = new(consoleStream)
+    {
+        AutoFlush = true
+    };
+
+    public void Info(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+         _writer.WriteLine($"({DateTime.Now.ToLocalTime()}) [LOG]: {message}");
+        Console.ResetColor();
+    }
+
+    public void Warn(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        _writer.WriteLine($"({DateTime.Now.ToLocalTime()}) [WARN]: {message}");
+        Console.ResetColor();
+    }
+
+    public void Error(string message, Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        _writer.WriteLine($"({DateTime.Now.ToLocalTime()}) [ERROR]: {ex.Message}");
+        _writer.WriteLine("Terminating!");
+        Console.ResetColor();
+        throw ex;
+    }
+
+    public void Debug(string message)
+    {
+        if (!writeDebug) return;
+        Console.ForegroundColor = ConsoleColor.Green;
+        _writer.WriteLine($"({DateTime.Now.ToLocalTime()}) [DEBUG]: {message}");
+        Console.ResetColor();
+    }
 }
